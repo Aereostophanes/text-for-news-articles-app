1f9fd3a047426682b42f01ceeab1ea6cb8f51960
Matthew James <aereostophanes@Matthews-MacBook-Pro-4.local>
2016-11-18 13:57:54 -0500

    Updating project repository.

---
+++ index.js
@@ -0,0 +1,18 @@
+var Hapi = require('hapi');
+var routes = require('./routes')(Hapi);
+var server = new Hapi.Server();
+
+server.connection({
+	host : 'localhost',
+	port : 3000 
+});
+
+server.route([{
+  method: 'GET',
+  path: '/vote/sms',
+  handler: routes.voteSMS
+}]);
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
